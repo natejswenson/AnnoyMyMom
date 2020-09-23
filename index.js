d75dffe0c5f6ae40400aa79617794447fdfba5e5
nate <nswenson@logisticshealth.com>
2020-09-22 20:18:40 -0700

    update encryption

--- index.js
+++ index.js
@@ -2,35 +2,22 @@
 
 'use strict';
 const Alexa = require('alexa-sdk');
-
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = 'amzn1.ask.skill.d6592a4d-aca0-4b53-9192-c274b958a44a';
+let APP_ID = proccess.env.APP_ID;
 
 const SKILL_NAME = 'Annoy my Mom';
 //const GET_FACT_MESSAGE = "Here's your fact: ";
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_MESSAGE = 'mommy why do you not understand what i am meant to do';
 const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'why are you leaving,why are you leaving me?';
+const STOP_MESSAGE = 'but mommy...why';
 const time ='<break time=".5s"/>';
 const card = 'annoy you agin later!';
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
+
 const data = [
     'mum'+time+'mom'+time+ 'mom' + time + 'mom' + time + 'mom' + time + 'mom' + time + 'mom' + time + 'mom',
     'mum'+time+'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy',
     'mom'+time+'mom'+time+ 'mom' + time+'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mama'+time+ 'mama'+time+ 'mama',
     'mommy'+time+ 'mama'+time+ 'mama'+ time + 'mama'+time+ 'mama'+time+ 'mama' + time + 'mama'+time+ 'mama'+time+ 'mama'
 ];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
 
 const handlers = {
     'LaunchRequest': function () {
