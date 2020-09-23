8b88e03357cf64c72bd40b4ec84387aeb416eb1a
nate <nswenson@logisticshealth.com>
2020-09-23 06:12:22 -0700

    update const data

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'but mommy...why';
 const time ='<break time=".5s"/>';
 const card = 'annoy you agin later!';
+const a = 'mum';
+const b = 'mom';
+const c = 'mommy';
+const d = 'mama';
+const e = 'please mommy please'
+
 
 const data = [
-    'mum'+time+'mom'+time+ 'mom' + time + 'mom' + time + 'mom' + time + 'mom' + time + 'mom' + time + 'mom',
-    'mum'+time+'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mommy',
-    'mom'+time+'mom'+time+ 'mom' + time+'mommy'+time+ 'mommy'+time+ 'mommy'+time+ 'mama'+time+ 'mama'+time+ 'mama',
-    'mommy'+time+ 'mama'+time+ 'mama'+ time + 'mama'+time+ 'mama'+time+ 'mama' + time + 'mama'+time+ 'mama'+time+ 'mama'
+    a + time + a + time + a + time + a + time + a + time + a + time + b + time + b,
+    a + time + b + time + b + time + b + time + c + time + d + time + a + time + b,,
+    b + time + c + time + c + time + c + time + e + time + d + time + d + time + b,,
+    c + time + b + time + c + time + b + time + a + time + e + time + b + time + b,
 ];
 
 const handlers = {
