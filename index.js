fad61bfad71b497e26ca8abc69562f0fb0f7a2ab
nate <nswenson@logisticshealth.com>
2020-09-22 18:46:28 -0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
+
 
 'use strict';
 const Alexa = require('alexa-sdk');
