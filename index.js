58ac1a8843ad9a6a8b6497fd5d9c73ee8dc77418
nate <swen407@gmail.com>
2020-12-13 15:27:45 -0600

    nate

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 'use strict';
 const Alexa = require('alexa-sdk');
-import util from './util.js';
-var AWS = require('aws-sdk');
 let APP_ID = process.env.APP_ID;
-let S3_A = process.env.S3_PERSISTENCE_BUCKET;
 const SKILL_NAME = 'Annoy my Mom';
 const STOP_MESSAGE = 'but mommy...why';
-const a = util.getS3PreSignedUrl('converted_audio20201213-23634-4k1rtz.mp3')
 var music = [ 
     '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1ev1rj4.mp3"/>',
     '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1kmqfdc.mp3"/>',
