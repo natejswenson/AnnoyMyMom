9681ee72aebde269856e63d7dca4e0d69c0a0be6
nate <swen407@gmail.com>
2020-12-13 09:56:46 -0600

    nate

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const SKILL_NAME = 'Annoy my Mom';
 const HELP_MESSAGE = 'mommy why do you not understand what i am meant to do';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'but mommy...why';
+const time ='<break time=".5s"/>';
+const card = 'annoy you agin later!';
+const a = 'mum';
+const b = 'mom';
+const c = 'mommy';
+const d = 'mama';
+const e = 'please mommy please'
+const f =' <audio src="https://www.jovo.tech/audio/Jz6MXZgh-r1m4a.mp3/> ';
 
+var music = [    
+    a + time + a + time + a + time + a + time + a + time + a + time + b + time + b,
+    a + time + b + time + b + time + b + time + c + time + d + time + a + time + b,
+    b + time + c + time + c + time + c + time + e + time + d + time + d + time + b,
+    c + time + b + time + c + time + b + time + a + time + e + time + b + time + b,
+    f
+];   
 
-
-var music = [   ' <audio src="https://www.jovo.tech/audio/Jz6MXZgh-r1m4a.mp3/> '
-]
 var random =function getRandomItem(array) {
 
     let i = 0;
@@ -23,12 +35,14 @@ const handlers = {
         this.emit('annoymyMomIntent');
     },
     'annoymyMomIntent': function () {
-      
-        const speechOutput =  '<audio src="https://www.jovo.tech/audio/Jz6MXZgh-r1m4a.mp3/>' ;
+        const factArr = data;
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+        const speechOutput = randomFact;
+        this.response.cardRenderer(SKILL_NAME, card);
         this.response.speak(speechOutput);
         this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent': function () {
+    }, function () {
         const speechOutput = HELP_MESSAGE;
         const reprompt = HELP_REPROMPT;
 
