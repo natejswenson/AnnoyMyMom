5074765ad5cd3ecedbc858c31ed0a77e37e5e396
nate <swen407@gmail.com>
2020-12-13 13:40:22 -0600

    nate

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 let APP_ID = process.env.APP_ID;
+let S3_A = process.env.S3_PERSISTENCE_BUCKET;
 const SKILL_NAME = 'Annoy my Mom';
 const STOP_MESSAGE = 'but mommy...why';
 var music = [ 
+    S3_A,
     '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1ev1rj4.mp3"/>',
     '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1kmqfdc.mp3"/>',
     '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-4k1rtz.mp3"/>'
