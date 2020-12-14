f8b6c4fa298933b613beed293a9edf65eeefed0e
nate <swen407@gmail.com>
2020-12-13 21:42:03 -0600

    update secrets

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 'use strict';
 const Alexa = require('alexa-sdk');
-const util = require('util');
 let APP_ID = process.env.APP_ID;
 let S31 = process.env.S3_1
 let S32 = process.env.S3_2
 let S33 = process.env.S3_3
+let S34 = process.env.S3_4
 const SKILL_NAME = 'Annoy my Mom';
 const STOP_MESSAGE = 'but mommy...why';
-var music =[ 
-    S31,
-    S32,
-    S33];
-  //  '<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1ev1rj4.mp3"/>',
-    //'<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-1kmqfdc.mp3"/>',
-    //'<audio src="https://alexa2018.s3.amazonaws.com/converted_audio20201213-23634-4k1rtz.mp3"/>'
-//];  
-
+var music =[ S31,S32,S33,S34]; 
 const handlers = {
     'LaunchRequest': function () {
         this.emit('annoymyMomIntent');
